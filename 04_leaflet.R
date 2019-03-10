af3bcee27fde51b2b4b667855484f85e6b10f7d3
lwawrowski <lukasz8989@gmail.com>
2019-03-10 20:58:54 +0100

    04_leaflet

---
+++ 04_leaflet.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(stplanr)
+library(leaflet)
+
+load("data/04_points_data.RData")
+
+# otwarte dane nt. Poznania
+
+
+# ikea
+# https://www.promoceny.pl/sklepy/szukaj/ikea/
+
+
+# bank

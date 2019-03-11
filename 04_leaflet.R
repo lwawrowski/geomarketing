1f851ff403acd122341a74a40846dd016b802fd9
lwawrowski <lukasz8989@gmail.com>
2019-03-11 08:26:43 +0000

    leaflet punkty

--- 04_leaflet.R
+++ 04_leaflet.R
@@ -6,9 +6,54 @@ load("data/04_points_data.RData")
 
 # otwarte dane nt. Poznania
 
+leaflet(biletomaty) %>%
+  addTiles() %>%
+  addMarkers(lng = ~Longitude, lat = ~Latitude, label = ~TVM)
+
+# geokodowanie
+
+a <- geo_code("al. niepodległości 10, poznań")
+
+b <- geo_code("al. niepodległości 10, poznań", return_all = TRUE)
 
 # ikea
 # https://www.promoceny.pl/sklepy/szukaj/ikea/
 
+ikea <- ikea %>%
+  mutate(lon=0,
+         lat=0)
+
+for(nr in 1:11){
+  
+  wsp <- geo_code(ikea$adres[nr])
+  
+  ikea$lon[nr] <- wsp[1]
+  ikea$lat[nr] <- wsp[2]
+  
+  Sys.sleep(1)
+}
+
+leaflet(ikea) %>%
+  addTiles() %>%
+  addMarkers(lng = ~lon, lat = ~lat, label = ~miasto)
 
 # bank
+
+bank <- bank %>%
+  mutate(adres_miasto=paste(adres, miejscowosc, sep=", "),
+         lon=0,
+         lat=0)
+
+for(nr in 1:nrow(bank)){
+  
+  wsp <- geo_code(bank$adres_miasto[nr])
+  
+  bank$lon[nr] <- wsp[1]
+  bank$lat[nr] <- wsp[2]
+  
+  Sys.sleep(1)
+}
+
+leaflet(bank) %>%
+  addTiles() %>%
+  addMarkers(lng = ~lon, lat = ~lat, label = ~adres)
